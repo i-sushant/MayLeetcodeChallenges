859df4327e0067e4886fa6dda1d447ce859bcc55
Sushant Gupta <imsushant25@gmail.com>
2020-05-03 19:39:04 +0530

    make it better

--- DayThree.java
+++ DayThree.java
@@ -1,24 +1,25 @@
 import java.util.*;
+
 class Solution {
-    public boolean canConstruct(String r, String magazine) {
+    public boolean canConstruct(String r, String m) {
         Map<Character, Integer> hmap = new HashMap<Character, Integer>();
-        for(int i =0; i<magazine.length();i++){
-            if(hmap.get(magazine.charAt(i)) == null){
-                hmap.put(magazine.charAt(i),1);
+        for (int i = 0; i < m.length(); i++) {
+            if (!hmap.containsKey(m.charAt(i))) {
+                hmap.put(m.charAt(i), 1);
             } else {
-                hmap.put(magazine.charAt(i), hmap.get(magazine.charAt(i)) + 1);
+                hmap.put(m.charAt(i), hmap.get(m.charAt(i)) + 1);
             }
         }
         int count = 0;
-        for(int i = 0; i<r.length(); i++){
-            if(hmap.get(r.charAt(i)) == null || hmap.get(r.charAt(i)) < 1){
+        for (int i = 0; i < r.length(); i++) {
+            if (hmap.containsKey(r.charAt(i)) || hmap.get(r.charAt(i)) < 1) {
                 return false;
             } else {
-                hmap.put(r.charAt(i) , hmap.get(r.charAt(i)) - 1);
+                hmap.put(r.charAt(i), hmap.get(r.charAt(i)) - 1);
                 count++;
             }
         }
-        if(count == r.length()){
+        if (count == r.length()) {
             return true;
         }
         return false;
