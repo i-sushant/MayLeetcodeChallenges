41919982d815bdea32e8197c62e6b8f52001bcc6
Sushant Gupta <imsushant25@gmail.com>
2020-05-10 20:54:17 +0530

    Day 10 added

--- Day10.java
+++ Day10.java
@@ -1,41 +1,13 @@
 class Solution {
     public int findJudge(int N, int[][] trust) {
-        if(N == 1){
-            return N;
+        int[] isTrusted = new int[N + 1];
+        for (int[] person : trust) {
+            isTrusted[person[0]]--;
+            isTrusted[person[1]]++;
         }
-        Map<Integer,Integer> freq = new HashMap<>();
-        Map<Integer, Integer> tmap = new HashMap<>();
-        int m = trust.length;
-        for(int i = 0; i<m;i++){
-            int a = trust[i][0];
-            int b = trust[i][1]; 
-            if(!freq.containsKey(b)){
-                freq.put(b,1);
-            } else {
-                freq.put(b, freq.get(b) + 1);
-            }
-        }
-        
-        Set<Map.Entry<Integer,Integer>> entries = freq.entrySet();
-        int maxFreq = N-1;
-        int count = 0;
-        int judge = 0;
-        for(Map.Entry<Integer,Integer> entry : entries){
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if(value == maxFreq){
-                judge = key;
-                count++;
-            }
-        }
-        boolean flag = true;
-        for(int i = 0; i<m;i++){
-            if(trust[i][0] == judge){
-                flag = false;
-            }
-        }
-        if(count == 1 && judge > 0 && flag){
-            return judge;
+        for (int i = 1; i < isTrusted.length; i++) {
+            if (isTrusted[i] == N - 1)
+                return i;
         }
         return -1;
     }
