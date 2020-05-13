50216f7abe4856ce2953da4ba9c0009c5d0e6dee
Sushant Gupta <imsushant25@gmail.com>
2020-05-13 23:12:44 +0530

    Day 13 added

---
+++ Day13.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int size = num.length();
+        if (size == k) {
+            return "0";
+        }
+        Stack<Character> stack = new Stack();
+        int counter = 0;
+        while (counter < size) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(counter)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(counter));
+            counter++;
+        }
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            char curr_char = stack.pop();
+            sb.append(curr_char);
+        }
+
+        sb.reverse();
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+}
