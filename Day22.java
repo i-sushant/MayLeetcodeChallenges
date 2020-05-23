2bf49540892091b580fe90727f30ea68266f4b34
Sushant Gupta <imsushant25@gmail.com>
2020-05-23 23:34:42 +0530

    Day 23 added

--- Day22.java
+++ Day22.java
@@ -1,6 +1,16 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
 class Solution {
     public String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap<>();
+        char a[] = s.toCharArray();
+        IntStream.range(0, a.length).mapToObj(i -> a[i]).forEach(ch -> map.put(ch, map.getOrDefault(ch, 0) + 1)); // using
+                                                                                                                  // java
+                                                                                                                  // 8
+                                                                                                                  // functional
+                                                                                                                  // interface
+
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
